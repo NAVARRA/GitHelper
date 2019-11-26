345de894ffdf461b78cc68dfc502012d3db79f75
NAVARRA <contactnavarra@gmail.com>
2019-11-26 12:29:50 +0100

    Add files via upload

--- githelper.py
+++ githelper.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#import tkinter
 from tkinter import *
 from tkinter import filedialog
 import tkinter.ttk as ttk
 import tkinter.messagebox as tkMessageBox
 import webbrowser, os , re , sys, time
 from os import path
-global pfad
+#global pfad
 
-stand = "GitHelper   Version: 1.0 Software Stand: 18.10.2019"
-NX = 8
-NY = 8
+stand = "GitHelper   Software Version: 1.0, 26.11.2019"
+
 class Application(ttk.Frame):
     
     def __init__(self,master=None):
         Frame.__init__(self,master)
         self.configure(background='black')
-        #self.rowconfigure(15, minsize='30')
-        #self.columnconfigure(15, minsize='30')
         self.parent = master
         self.master = master
         self.tool_bar()
         self.Werbung()
         self.hintergrund()
-        #self.create_test_canvas()
-        #self.place() 
-        
         self.grid()
         
         self.pfad=str(os.getcwd())
@@ -54,10 +47,9 @@ class Application(ttk.Frame):
         self.varTN1 = StringVar()
         self.varTN1.set("current path")
         self.labelTN1 = Label(self,textvariable=self.varTN1,height = 1,bg='black',fg='SteelBlue1',font='impact 15',relief='flat')
-        self.labelTN1.grid(row=9,column=5,columnspan=NX)
-        #self.pfad="path"
+        self.labelTN1.grid(row=9,column=4,columnspan=8)
         self.boxTN1=Entry(self,textvariable=self.pfad,width=80,bg='grey',fg='SteelBlue1',font='impact 10',relief='flat')
-        self.boxTN1.grid(row=10,column=0,columnspan=NX)
+        self.boxTN1.grid(row=10,column=0,columnspan=8)
         
         self.varTN2 = StringVar()
         self.varTN2.set("Message ")
@@ -89,15 +81,15 @@ class Application(ttk.Frame):
             
     def git_status(self):
         print ("git status")
-        #os.system('git status')
+        os.system('git status')
         self.display()
     def git_pull(self):
         print ("git pull") 
-        #os.system('git pull')
+        os.system('git pull')
         self.display()
     def git_add(self):
         print ("git add -A")
-        #os.system('git add -A')
+        os.system('git add -A')
         self.display()
     def git_commit(self):
         self.message = self.boxTN2.get()
@@ -107,11 +99,11 @@ class Application(ttk.Frame):
         else:
             self.display()
             print ("git commit -m " + '"' + self.message + '"')
-        #os.system('git clone + '"' + self.meassage + '"')
+            os.system('git clone + '"' + self.meassage + '"'')
         self.display()
     def git_push(self):
         print ("git push") 
-        #os.system('git push')
+        os.system('git push')
         self.display()
         
     def tool_bar(self):
@@ -123,15 +115,12 @@ class Application(ttk.Frame):
         file.add_command(label="Exit", command=self.quit)
         menu.add_cascade(label="Datei", menu=file)
         
-        #edit = Menu(menu)
-        #edit.add_command(label="öffne neue *.Ino Datei", command=self.open_new_ino)
-        #menu.add_cascade(label="*.Ino Datei", menu=edit)
-        
         about = Menu(menu)
         about.add_command(label="Navarra Software Solutions", command=self.about)
         menu.add_cascade(label="About", menu=about)
     
     def quit(self):
+        print ("I hope you enjoyed using my Tool!")
         sys.exit(0)
             
     def about(self):
@@ -147,16 +136,16 @@ class Application(ttk.Frame):
         Label(self.about, text=(self.SLOGAN) , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=130,y=60);
         Label(self.about, text=(self.WEBSITE), bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=130,y=100);
         Label(self.about, text=(self.MAIL)   , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=130,y=140);
-        Label(self.about, text=(stand)  , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) +  ("8"))).place(x=250,y=230);
+        Label(self.about, text=(stand)       , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) +  ("8"))).place(x=250,y=230);
     
     def hintergrund(self):
-        Label(self, text=str("[") + ("   ") + str("]"),bg=(self.BG),fg='grey',font='Helvetica 50 bold ').place(x=350,y=110);
-        Label(self, text=str("*"),bg=(self.BG),fg=(self.FGBLUE),font=((self.FONT) + ("124"))).place(x=380,y=100);
+        Label(self, text=str("[") + ("   ") + str("]"),bg=(self.BG),fg='grey',font='Helvetica 50 bold ').place(x=320,y=70);
+        Label(self, text=str("*"),bg=(self.BG),fg=(self.FGBLUE),font=((self.FONT) + ("124"))).place(x=350,y=60);
         
-        #Label(self, text=(self.NAVARRA), bg=(self.BG), fg=(self.FGBLUE),font=((self.FONT) + ("22"))).grid(row=3,column=10,rowspan=3,columnspan=3);
-        #Label(self, text=(self.SLOGAN) , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=160);
-        #Label(self, text=(self.WEBSITE), bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=200);
-        #Label(self, text=(self.MAIL)   , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=240);
+        Label(self, text=(self.NAVARRA), bg=(self.BG), fg=(self.FGBLUE),font=((self.FONT) + ("22"))).grid(row=3,column=10,rowspan=3,columnspan=3);
+        Label(self, text=(self.SLOGAN) , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=160);
+        Label(self, text=(self.WEBSITE), bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=200);
+        Label(self, text=(self.MAIL)   , bg=(self.BG), fg=(self.FG)    ,font=((self.FONT) + ("14"))).place(x=255,y=240);
     
     def display(self):
         
@@ -168,8 +157,7 @@ class Application(ttk.Frame):
         self.boxTN3.insert(0,(self.clone_repo))
         
     def Werbung(self):
-        
-        self.LOGO    = "[*]"
+        self.LOGO    = "[*] "
         self.NAVARRA = "NAVARRA SOFTWARE SOLUTIONS"
         self.BG      = 'black'
         self.FG      = 'white'
@@ -179,22 +167,22 @@ class Application(ttk.Frame):
         self.WEBSITE = "https://NAVARRA.home.blog"
         self.MAIL    = "contactNAVARRA@gmail.com"
         self.CEO     = "Sebastian de la Vega"
-        self.PHONE   = "Phone: +49 (0) 151 566 57246 "
+        self.INSTAG  = "Instagram @contactnavarra"
         
         def show_werbung(self):
             print (stand)
             print ("")
-            print ("Grafisches Tool um das arbeiten mit GIT zu erleichtern")
+            print ("GUI to make working with Git a little easier")
             print ("")
-            print ("von:")
-            print (self.NAVARRA)
+            print ("from:")
+            print (self.LOGO, self.NAVARRA)
             print (self.SLOGAN)
             print (self.WEBSITE)
             print (self.MAIL)
             print (self.CEO)
-            print (self.PHONE)
+            print (self.INSTAG)
             print ("\n")
-        #self.show_werbung()
+        show_werbung(self)
         
     
 #-------------------------------------------------------------------------------
@@ -203,8 +191,8 @@ class Application(ttk.Frame):
 def main():
     root = Tk()
     root.title("GitHelper")
-    root.resizable(True, True)
-    #root.resizable(False, False)
+    #root.resizable(True, True)
+    root.resizable(False, False)
     root.configure(background='black')
     root.wm_overrideredirect(False)
     root.geometry("800x500")
